cd497f2052f44e2610ee5f898f3501c0f5dfba62
LokeshV790 <lokeshvazirani29@gmail.com>
2024-03-20 08:54:09 +0530

    Portfolio: Background particles
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Particles from "@/components/Particles";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={150}
+      />
     </main>
   );
 }
